16d216d8a633d822fcc720b99f5c5674c7e204cd
Ailton Rodrigues <ailtonrodrigues1324@hotmail.com>
2024-07-16 14:46:51 -0300

    melhorias na documentação

--- usandoModulos/main.py
+++ usandoModulos/main.py
@@ -2,14 +2,17 @@ from mymodule import *
 
 
 def view_result(message):
+    """
+    Função que recebe uma mensagem e a imprime na tela.
+    :param message: Fornece a mensagem a ser impressa na tela.
+    """
     print(message)
 
 
 def trate_dict():
     """
-    Gera o dicionário entered_values com 10 valores aleatórios e passa esse dicionário para a função process_dict,
-    que retorna a soma total, a média e a variação dos valores.
-    Esses valores são exibidos na tela usando a função view_result.
+    Gera um dicionário com 10 valores aleatórios e passa esse dicionário para a função process_dict,
+    que retorna um dicionário contendo a soma total, a média e a variação desses valores.
     """
     entered_values = {'value_' + str(i): generate_numbers() for i in range(10)}
     trated_dictionary = process_dict(entered_values)
@@ -23,7 +26,6 @@ def convert_celsius_to_fahrenheit():
     """
     Gera um valor aleatório entre 0 e 100 e passa esse valor para a função c_to_f, que retorna o valor convertido
     para fahrenheit.
-    O resultado é exibido na tela usando a função view_result.
     """
     celsius = generate_numbers(0, 100)
     view_result(f"O resultado da conversão de {celsius}ºC para fahrenheit é {c_to_f(celsius)}ºF\n")
@@ -33,7 +35,6 @@ def convert_fahrenheit_to_celsius():
     """
     Gera um valor aleatório entre 0 e 212 e passa esse valor para a função f_to_c, que retorna o valor convertido
     para celsius.
-    O resultado é exibido na tela usando a função view_result.
     """
     fahrenheit = generate_numbers(0, 212)
     view_result(f"O resultado da conversão de {fahrenheit}ºF para Celsius é {f_to_c(fahrenheit)}ºC\n")
@@ -43,7 +44,6 @@ def verify_prime_numbers():
     """
     Gera uma lista de 100 números aleatórios entre 0 e 1000 e passa essa lista para a função primes, que retorna
     os números primos encontrados.
-    O resultado é exibido na tela usando a função view_result.
     """
     numbers_for_verify_prime = [generate_numbers(1, 1000) for _ in range(100)]
     view_result(f"Na lista de 100 números aleatórios entre 0 e 1000, foram encontrados os sequintes e números primos:\n"
@@ -54,7 +54,6 @@ def items_in_unic_plan():
     """
     Gera uma lista de 4 listas com 4 valores aleatórios e passa essa lista para a função sort_items, que retorna
     os valores concatenados e ordenados em ordem crescente.
-    O resultado é exibido na tela usando a função view_result.
     """
     items_group = [[generate_numbers() for _ in range(4)] for _ in range(4)]
     view_result(f"Aqui está a lista {items_group} concatenada e organizada em um unico plano:\n"
@@ -65,7 +64,6 @@ def phare_in_reverse_plan():
     """
     Gera uma lista com uma frase e passa essa lista para a função reverse, que retorna a frase com cada palavra
     escrita de forma inversa.
-    O resultado é exibido na tela usando a função view_result.
     """
     phrase = 'ailton cruz rodrigues'.split()
     view_result(f"A frase {phrase} com cada palavra escrita inversamente é: {reverse(phrase)}\n")
@@ -75,7 +73,6 @@ def tuple_in_random_order():
     """
     Gera uma lista de 4 tuplas com até 4 valores aleatórios. Passa essa lista para a função random_order,que concatena
     os valores das tuplas e os retorna em uma lista ordenada de forma aleatória (cresecente ou decrescente)
-    O resultado é exibido na tela usando a função view_result.
     """
     tuple_listing = [tuple(generate_numbers() for _ in range(generate_numbers(1, 10))) for _ in range(4)]
     view_result(f"A lista de tuplas fornecisda foi: {tuple_listing}.\n"
